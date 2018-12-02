f57e612fdce91610ebeb161702034e721825f991
Ximinhan <1921745321@qq.com>
2018-12-02 13:13:47 +0800

    try to use zh_cn

--- source/conf.py
+++ source/conf.py
@@ -63,7 +63,7 @@ release = u'1.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = zh_CN
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -106,7 +106,7 @@ html_sidebars = {
         'navigation.html',
         'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
-        'donate.html',
+        #'donate.html',
     ]
 }
 
